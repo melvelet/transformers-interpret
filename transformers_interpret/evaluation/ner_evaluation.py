65a42e96bdb22ee4c99d4f24cb450d736502fb80
Richard Herrmann <45592339+melvelet@users.noreply.github.com>
2022-06-30 13:32:45 +0200

    calculate stats of entities not sentence averages

--- transformers_interpret/evaluation/ner_evaluation.py
+++ transformers_interpret/evaluation/ner_evaluation.py
@@ -8,6 +8,12 @@ from transformers import PreTrainedModel, PreTrainedTokenizer, Pipeline
 from transformers_interpret import TokenClassificationExplainer
 
 
+def get_rationale(attributions, k: int, continuous: bool = False, return_mask: bool = False):
+    if continuous:
+        return get_continuous_rationale(attributions, k, return_mask)
+    return get_topk_rationale(attributions, k, return_mask)
+
+
 def get_topk_rationale(attributions, k: int, return_mask: bool = False):
     if len(attributions) == 0:
         return []
@@ -23,9 +29,19 @@ def get_topk_rationale(attributions, k: int, return_mask: bool = False):
     return indices.tolist()
 
 
-def calculate_compsuff(comprehensiveness: Dict, sufficiency: Dict):
-    return {k: comprehensiveness[k] - sufficiency[k]
-            for k in comprehensiveness}
+def get_continuous_rationale(attributions, k: int, return_mask: bool = False):
+    if len(attributions) == 0 or return_mask:
+        return []
+
+    tensor = torch.FloatTensor([a[1] for a in attributions])
+    scores: List[float] = list()
+    for i, _ in enumerate(tensor[:len(tensor) - k + 1]):
+        scores.append(sum(tensor[i:i+k]))
+
+    argmax = torch.argmax(torch.FloatTensor(scores)).item()
+    indices = [i for i in range(argmax, argmax + k)]
+
+    return indices
 
 
 class NERSentenceEvaluator:
@@ -57,10 +73,11 @@ class NERSentenceEvaluator:
             e['attribution_scores'] = word_attributions[e['entity']][e['index']]
             e['comprehensiveness'] = dict()
             e['sufficiency'] = dict()
+            e['rationales'] = dict()
 
-    def calculate_comprehensiveness(self, k):
+    def calculate_comprehensiveness(self, k: int, continuous: bool = False):
         for e in self.entities:
-            rationale = get_topk_rationale(e['attribution_scores'], k)
+            rationale = get_rationale(e['attribution_scores'], k, continuous)
             masked_input = torch.tensor([self.input_token_ids])
             for i in rationale:
                 masked_input[0][i] = self.tokenizer.mask_token_id
@@ -69,9 +86,9 @@ class NERSentenceEvaluator:
             new_conf = scores[e['index']][self.label2id[e['entity']]].item()
             e['comprehensiveness'][k] = e['score'] - new_conf
 
-    def calculate_sufficiency(self, k):
+    def calculate_sufficiency(self, k: int, continuous: bool = False):
         for e in self.entities:
-            rationale = get_topk_rationale(e['attribution_scores'], k)
+            rationale = get_rationale(e['attribution_scores'], k, continuous)
             masked_input = torch.tensor([self.input_token_ids])
             for i, _ in enumerate(masked_input[0][1:-1]):
                 if i + 1 not in rationale:
@@ -83,53 +100,33 @@ class NERSentenceEvaluator:
             # print('old_conf', e['score'], 'new_conf', new_conf, 'old_label', e['entity'], 'new_label', new_label, 'diff', e['score'] - new_conf)
             e['sufficiency'][k] = e['score'] - new_conf
 
-    def calculate_average_scores_for_sentence(self):
-        def _calculate_mean(attr: str, squared: bool = False):
-            entities = self.entities[0][attr] if self.entities else []
-            if squared:
-                return {k: mean([e[attr][k]**2 for e in self.entities])
-                        for k in entities}
-            return {k: mean([e[attr][k] for e in self.entities])
-                    for k in entities}
+    def write_rationales(self, k: int, continuous: bool = False):
+        for e in self.entities:
+            e['rationales'][k] = get_rationale(e['attribution_scores'], k, continuous)
 
-        if self.entities is None \
-                or (len(self.entities) > 0
-                    and (len(self.entities[0]['comprehensiveness']) == 0
-                    or len(self.entities[0]['sufficiency']) == 0)):
-            raise ValueError("Scores have not yet been calculated. Please call the evaluator on text first.")
+    def get_all_scores_in_sentence(self, k_values):
+        document_scores = list()
+        for e in self.entities:
+            entity_scores = {
+                'comprehensiveness': {k: e['comprehensiveness'][k] for k in k_values},
+                'sufficiency': {k: e['sufficiency'][k] for k in k_values},
+                'compdiff': {k: e['comprehensiveness'][k] - e['sufficiency'][k] for k in k_values},
+            }
+            document_scores.append(entity_scores)
 
-        if len(self.entities) == 0:
-            return None
+        return document_scores
 
-        return {
-            'mean': {
-                'comprehensiveness': _calculate_mean('comprehensiveness'),
-                'sufficiency': _calculate_mean('sufficiency'),
-                'compdiff': calculate_compsuff(
-                    comprehensiveness=_calculate_mean('comprehensiveness'),
-                    sufficiency=_calculate_mean('sufficiency'),
-                )
-            },
-            'squared_mean': {
-                'comprehensiveness': _calculate_mean('comprehensiveness', squared=True),
-                'sufficiency': _calculate_mean('sufficiency', squared=True),
-                'compdiff': calculate_compsuff(
-                    comprehensiveness=_calculate_mean('comprehensiveness', squared=True),
-                    sufficiency=_calculate_mean('sufficiency', squared=True),
-                )
-            },
-        }
-
-    def __call__(self, input_: str, k_values: List[int] = [1]):
+    def __call__(self, input_: str, k_values: List[int] = [1], continuous: bool = False):
         self.input_str = input_
         self.execute_base_classification()
         self.calculate_attribution_scores()
         for k in k_values:
-            self.calculate_comprehensiveness(k)
-            self.calculate_sufficiency(k)
+            self.write_rationales(k, continuous=continuous)
+            self.calculate_comprehensiveness(k, continuous=continuous)
+            self.calculate_sufficiency(k, continuous=continuous)
 
         return {
-            'scores': self.calculate_average_scores_for_sentence(),
+            'scores': self.get_all_scores_in_sentence(k_values),
             'entities': self.entities,
             'tokens': len(self.input_tokens),
         }
@@ -162,9 +159,9 @@ class NERDatasetEvaluator:
 
             try:
                 if squared:
-                    return {k: func([e['squared_mean'][attr][k]**2 for e in self.raw_scores if e is not None])
+                    return {k: func([e[attr][k]**2 for e in self.raw_scores])
                             for k in k_values}
-                return {k: func([e['mean'][attr][k] for e in self.raw_scores if e is not None])
+                return {k: func([e[attr][k] for e in self.raw_scores])
                         for k in k_values}
             except StatisticsError:
                 print(f"Can't calculate {func.__name__}. Too few data points...")
@@ -173,42 +170,35 @@ class NERDatasetEvaluator:
         if len(self.raw_scores) == 0:
             raise ValueError("Scores have not yet been calculated. Please call the evaluator on a dataset first.")
 
-        # Check if at least one sentence has a score
-        if not any(self.raw_scores):
-            return None
-
         return {
             'mean': {
                 'comprehensiveness': _calculate_statistical_function('comprehensiveness'),
                 'sufficiency': _calculate_statistical_function('sufficiency'),
-                'compdiff': calculate_compsuff(
-                    comprehensiveness=_calculate_statistical_function('comprehensiveness'),
-                    sufficiency=_calculate_statistical_function('sufficiency'),
-                )
+                'compdiff': _calculate_statistical_function('compdiff'),
             },
             'squared_mean': {
                 'comprehensiveness': _calculate_statistical_function('comprehensiveness', squared=True),
                 'sufficiency': _calculate_statistical_function('sufficiency', squared=True),
-                'compdiff': calculate_compsuff(
-                    comprehensiveness=_calculate_statistical_function('comprehensiveness', squared=True),
-                    sufficiency=_calculate_statistical_function('sufficiency', squared=True),
-                )
+                'compdiff': _calculate_statistical_function('compdiff', squared=True),
             },
             'median': {
                 'comprehensiveness': _calculate_statistical_function('comprehensiveness', func='median'),
                 'sufficiency': _calculate_statistical_function('sufficiency', func='median'),
+                'compdiff': _calculate_statistical_function('compdiff', func='median'),
             },
             'stdev': {
                 'comprehensiveness': _calculate_statistical_function('comprehensiveness', func='stdev'),
                 'sufficiency': _calculate_statistical_function('sufficiency', func='stdev'),
+                'compdiff': _calculate_statistical_function('compdiff', func='stdev'),
             },
             'variance': {
                 'comprehensiveness': _calculate_statistical_function('comprehensiveness', func='variance'),
                 'sufficiency': _calculate_statistical_function('sufficiency', func='variance'),
+                'compdiff': _calculate_statistical_function('compdiff', func='variance'),
             },
         }
 
-    def __call__(self, k_values: List[int] = [1]):
+    def __call__(self, k_values: List[int] = [1], continuous: bool = False):
         passages = 0
         entities = 0
         tokens = 0
@@ -216,16 +206,16 @@ class NERDatasetEvaluator:
         start_time = datetime.now()
         for split in self.dataset:
             for document in split:
-                # if passages > 5:
-                #     break
+                if passages > 5:
+                    break
                 for passage in document['passages']:
                     passages += 1
                     print('Passage', passages, end='\r', flush=True)
                     if len(passage['text']) > 1:
                         print('len(passage[\'text\']) > 1', passage)
                         exit(-1)
-                    result = self.evaluator(passage['text'][0], k_values)
-                    self.raw_scores.append(result['scores'])
+                    result = self.evaluator(passage['text'][0], k_values, continuous)
+                    self.raw_scores.extend(result['scores'])
                     self.raw_entities.append(result['entities'])
                     entities += len(result['entities'])
                     if len(result['entities']) == 0:
@@ -248,9 +238,11 @@ class NERDatasetEvaluator:
             'settings': {
                 'model': self.pipeline.model.config._name_or_path,
                 'tokenizer': self.pipeline.tokenizer.name_or_path,
-                'dataset': [dataset.info.config_name for dataset in self.dataset],
+                'dataset': set([dataset.info.config_name for dataset in self.dataset]),
+                'splits': [split for split in self.dataset[0].info.splits],
                 'attribution_type': self.attribution_type,
                 'k_values': k_values,
+                'continuous': continuous,
             },
             'timing': {
                 'start_time': str(start_time),
