f2317c24124141daa0ea4cdc85632484cedca1ee
Richard Herrmann <45592339+melvelet@users.noreply.github.com>
2022-07-08 18:59:57 +0200

    add print

--- finetune_model.py
+++ finetune_model.py
@@ -7,9 +7,9 @@ from transformers_interpret.evaluation.input_pre_processor import InputPreProces
 
 
 def compute_metrics(eval_pred):
-    print(eval_pred)
     logits, labels = eval_pred
     predictions = np.argmax(logits, axis=-1)
+    print(predictions)
     return metric.compute(predictions=predictions, references=labels)
 
 
