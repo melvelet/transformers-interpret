cfc4a154bf5f2a444a505c3a621b0f4fa55e1dfe
Richard Herrmann <45592339+melvelet@users.noreply.github.com>
2022-10-12 16:05:34 +0200

    .

--- get_f1_scores.py
+++ get_f1_scores.py
@@ -25,7 +25,7 @@ metric = load_metric("seqeval")
 
 def compute_metrics(eval_pred):
     logits, labels = eval_pred
-    print(logits.logits.shape)
+    print(logits.shape)
     # print(logits)
     predictions = np.argmax(logits, axis=-1)
     print(predictions)
@@ -98,7 +98,7 @@ for dataset_name in dataset_names:
         # model_predictions = [model(torch.tensor(doc['input_ids'])) for doc in tokenized_datasets]
         model_predictions = model(input_ids)
         gold_references = torch.tensor([doc['labels'] for doc in tokenized_datasets])
-        final_score = compute_metrics((model_predictions, gold_references))
+        final_score = compute_metrics((model_predictions.logits, gold_references))
         # final_score = metric.compute(predictions=model_predictions, references=gold_references)
         final_score['model_name'] = model_name
         final_score['dataset_name'] = dataset_name
