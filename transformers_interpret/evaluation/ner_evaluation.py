a49992a0205b3ee3e9834d5a1b1cfb5d0be075e1
Richard Herrmann <45592339+melvelet@users.noreply.github.com>
2022-10-13 14:29:18 +0200

    .

--- transformers_interpret/evaluation/ner_evaluation.py
+++ transformers_interpret/evaluation/ner_evaluation.py
@@ -4,6 +4,7 @@ from typing import List, Dict, Union, Optional
 
 import numpy as np
 import torch
+from alive_progress import alive_bar
 from datasets import Dataset
 from transformers import PreTrainedModel, PreTrainedTokenizer, Pipeline
 from transformers_interpret.evaluation.input_pre_processor import get_labels_from_dataset
@@ -211,25 +212,27 @@ class NERDatasetAttributor:
         tokens = 0
         start_time = datetime.now()
 
-        for document in self.dataset:
-            documents += 1
-            if start_document and documents < start_document:
-                continue
-            if max_documents and 0 < max_documents < documents:
-                break
-            print('Document', documents)
-            result = self.attributor(document,
-                                     evaluate_other=evaluate_other)
-            self.entities.append({
-                'entities': result['entities'],
-                'document_id': document['document_id'],
-                'discarded_entities': result['discarded_entities'],
-            })
-            discarded_entities += result['discarded_entities']
-            self.attributed_entities += len([e for e in result['entities']])
-            self.attributed_entities += len(
-                [e['other_entity'] for e in result['entities'] if e['other_entity'] is not None])
-            tokens += result['tokens']
+        with alive_bar(total=len(self.dataset)) as bar:
+            for document in self.dataset:
+                documents += 1
+                if start_document and documents < start_document:
+                    continue
+                if max_documents and 0 < max_documents < documents:
+                    break
+                print('Document', documents)
+                result = self.attributor(document,
+                                         evaluate_other=evaluate_other)
+                self.entities.append({
+                    'entities': result['entities'],
+                    'document_id': document['document_id'],
+                    'discarded_entities': result['discarded_entities'],
+                })
+                discarded_entities += result['discarded_entities']
+                self.attributed_entities += len([e for e in result['entities']])
+                self.attributed_entities += len(
+                    [e['other_entity'] for e in result['entities'] if e['other_entity'] is not None])
+                tokens += result['tokens']
+                bar()
 
         end_time = datetime.now()
         duration = end_time - start_time
