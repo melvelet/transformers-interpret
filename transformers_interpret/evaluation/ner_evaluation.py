40ff2cfd53d1dd1a3fcb4263a176462cd1935598
Richard Herrmann <45592339+melvelet@users.noreply.github.com>
2022-10-14 13:12:15 +0200

    .

--- transformers_interpret/evaluation/ner_evaluation.py
+++ transformers_interpret/evaluation/ner_evaluation.py
@@ -479,7 +479,6 @@ class NERDatasetEvaluator:
                  ):
         self.pipeline = pipeline
         self.pipeline.model.to(CUDA_DEVICE)
-        self.pipeline.tokenizer.to(CUDA_DEVICE)
         self.dataset = dataset
         self.label2id, self.id2label = get_labels_from_dataset(dataset, has_splits=False)
         print('label2id', self.label2id)
