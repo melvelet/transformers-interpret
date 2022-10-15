f5573c9f5cd082625c84a6b9068bce49afb152db
Richard Herrmann <45592339+melvelet@users.noreply.github.com>
2022-10-15 14:13:31 +0200

    .

--- transformers_interpret/evaluation/ner_evaluation.py
+++ transformers_interpret/evaluation/ner_evaluation.py
@@ -325,10 +325,6 @@ class NERSentenceEvaluator:
                                                   bottom_k=bottom_k if not continuous else False)
                         # print(rationale, len(self.input_token_ids), 'bottom_k', bottom_k, 'continuous', continuous)
                         for j in rationale:
-                            print('len(masked_inputs)', len(masked_inputs))
-                            print('len(self.input_token_ids)', len(self.input_token_ids))
-                            print('rationale', rationale)
-                            print(len(masked_inputs[i]))
                             masked_inputs[i][j] = self.tokenizer.mask_token_id
                     elif measure == 'sufficiency':
                         rationale = get_rationale(e[f'{prefix}attribution_scores'], k, continuous,
