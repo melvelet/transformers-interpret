4df9b75ff22522b44cf39f0578acaba7a3a89b68
Richard Herrmann <45592339+melvelet@users.noreply.github.com>
2022-10-19 19:04:43 +0200

    .

--- generate_qualitative_viz.py
+++ generate_qualitative_viz.py
@@ -30,7 +30,7 @@ viz.load_dataset(dataset=args.dataset_no)
 viz.load_tokenizers(models=huggingface_models)
 viz.load_other_pipeline(base_path='./trained_models/')
 viz.load_entities(base_path='./results/scores/')
-viz.prepare(doc_id=doc_id, ref_token_idx=mod1_ref_token_idx)
+viz.prepare(doc_id=doc_id, ref1_token_idx=mod1_ref_token_idx, ref2_token_idx=mod2_ref_token_idx)
 with torch.no_grad():
     viz.ensure_attr_scores_in_other_model(mod2_ref_token_idx)
 viz.print_table(k_value=k_value)
