9160ceb9d968ed60c8d5afe613904aa0f6e01038
Richard Herrmann <45592339+melvelet@users.noreply.github.com>
2022-10-20 19:30:40 +0200

    .

--- sample_qualitative_viz.py
+++ sample_qualitative_viz.py
@@ -196,7 +196,7 @@ class QualitativeVisualizer:
         #         self.entity = e
         # assert self.entity is not None
         self.entity = [e for e in self.entities[self.huggingface_models[0]][attribution_types[0]]
-                       if e['doc_doc_id'] == str(doc_id) and e['index'] == ref1_token_idx][0]
+                       if _compare(e)][0]
         doc = [doc for doc in self.dataset if doc['document_id'] == doc_id][0]
         self.docs = {
             'bioelectra-discriminator': self.pre_processors['bioelectra-discriminator'](doc),
